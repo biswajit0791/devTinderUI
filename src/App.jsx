d625195bb45376f162609b4e7392e0129c855b42
Biswajit Parida <biswajitparida0791@gmail.com>
2025-09-06 23:29:12 +0530

    Create store and login api integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import Body from "./page/Body";
 import Login from "./page/Login";
 import Profile from "./page/Profile";
 import SignUp from "./page/SignUp";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./page/Feed";
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
